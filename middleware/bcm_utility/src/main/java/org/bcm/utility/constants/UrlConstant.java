fcb6bb1e2b520a883f8b7fde10c59c9a40929298
Sachin Tyagi <sachin.tyagi@soprasteria.com>
2019-06-06 10:05:57 +0530

    updated structure.

---
+++ middleware/bcm_utility/src/main/java/org/bcm/utility/constants/UrlConstant.java
@@ -0,0 +1,16 @@
+package org.bcm.utility.constants;
+
+public class UrlConstant {
+
+	private UrlConstant() {
+	}
+
+	/* Constants used within bmc_middleware module */
+	public static final String MIDDLEWARE = "/middleware";
+	public static final String MIDDLEWARE_GREETING = "/greeting";
+	
+	
+	/* Constants used within bmc_webapp_mw module */
+	public static final String WEBAPP = "/webapp";
+	public static final String WEBAPP_GREETING = "/greeting";
+}
--- middleware/bcm_webapp_mw/src/main/java/org/bcm/webapp/controller/SpringController.java
+++ middleware/bcm_webapp_mw/src/main/java/org/bcm/webapp/controller/SpringController.java
@@ -1,5 +1,8 @@
 package org.bcm.webapp.controller;
 
+import org.bcm.utility.constants.UrlConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/webapp")
+@RequestMapping(value = UrlConstant.WEBAPP)
 public class SpringController extends AbstractController {
 
-	@GetMapping("/greeting")
+	private static final Logger log = LoggerFactory.getLogger(SpringController.class);
+
+	@GetMapping(value = UrlConstant.WEBAPP_GREETING)
 	public ResponseEntity<? extends Object> getGreeting() {
+		log.info("bcm_webapp_mw: webapp_greeting executed ..........");
 		String body = "bcm_middleware: hello greeting";
 		return responseEntity(body, HttpStatus.OK);
 	}
